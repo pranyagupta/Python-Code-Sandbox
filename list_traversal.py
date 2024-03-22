70bc82fe180a9e54019ddcc5bd429f8bdeff53e4
Pranya@1234 <pranyagupta24.5j@gmail.com>
2024-03-22 15:43:25 +0530

    Add list_traversal file to the repo

---
+++ list_traversal.py
@@ -0,0 +1,15 @@
+pi = 3.14
+
+l = [True,'pranya',23,31.4,'chetan', pi]
+
+for item in l:
+    print(item)
+
+for stupid in range(len(l)):
+    print(l[stupid])
+
+i=0
+while (i<len(l)):
+    print(l[i])
+    i+=1
+
