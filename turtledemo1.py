da54ca6d1659dc1532f496f1dfd3d6586f57f795
Pranya@1234 <pranyagupta24.5j@gmail.com>
2024-04-13 12:46:13 +0530

    add a turtledemo file to the repo

---
+++ turtledemo1.py
@@ -0,0 +1,17 @@
+import turtle
+
+wn = turtle.Screen()
+
+pingu = turtle.Turtle()
+pingu.shape("turtle")
+
+pingu.left(90)
+pingu.fd(100)
+pingu.right(90)
+pingu.fd(30)
+pingu.right(90)
+pingu.fd(30)
+pingu.right(90)
+pingu.fd(30)
+
+wn.exitonclick()
