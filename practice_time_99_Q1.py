52ef59b9da851222ac27da765496bdb5450d7f91
Pranya@1234 <pranyagupta24.5j@gmail.com>
2024-04-14 13:14:45 +0530

    add a practice file to the repository

---
+++ practice_time_99_Q1.py
@@ -0,0 +1,4 @@
+amount=int(input("enter the cost of stationary you got:"))
+discount=(5/100)*amount
+print("you get a discount 5% that is now Rs.",discount)
+print('you have to pay',amount-discount)
