3a063e776389aac4e79be88b0d1a259ce079b002
Pranya@1234 <pranyagupta24.5j@gmail.com>
2024-07-17 12:54:28 +0530

    add files tp repo

---
+++ practice_demo.py
@@ -0,0 +1,19 @@
+#for i in range (1,11):
+  #  if i%2==0:
+ #       print(i)
+
+
+#n=100
+#print(n*(n+1)/2)
+sum=0
+i=1
+while(i<=10):
+    sum=sum+i
+    i=i+1
+    
+print(sum)
+i=0
+while(i<=10):
+    if i%2==0:
+        print(i)
+        i=i+1
